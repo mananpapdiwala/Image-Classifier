4f7cd4529e21e3644d189cd3a95cbe0caf766cab
Pratik <jainps@iu.edu>
2016-12-08 02:55:07 -0500

    trivial changes

--- imageClassifier.py
+++ imageClassifier.py
@@ -10,7 +10,7 @@ class ImageFiles:
     train_files = {}
 
 
-def read_files(file_name, train_mode):
+def read_files(file_name):
     files = {}
     input_file = open(file_name, 'r')
     for line in input_file:
@@ -31,7 +31,6 @@ def read_files(file_name, train_mode):
         files[data[0] + data[1]] = {"orient": int(data[1]), "img": img}
 
     input_file.close()
-    print len(files)
     return files
 
 
@@ -40,13 +39,8 @@ def test_nearest():
     i = 0
     result = 0
     for test_f_id in imf.test_files:
-        print i
         i += 1
         test_f_img = imf.test_files[test_f_id]["img"]
-
-        # when k = 20
-        # min_dist = [maxint]*20
-        # img_with_min_dist = [""]*20
 
         min_dist = maxint
         img_with_min_dist = ""
@@ -56,24 +50,10 @@ def test_nearest():
             new_img = np.subtract(train_f_img, test_f_img)
             new_img = np.square(new_img)
             dist = np.sum(new_img)
-            """ if dist < min_dist[19]:
-                min_dist[19] = dist
-                img_with_min_dist[19] = train_f_id
-                img_with_min_dist = [x for (y, x) in sorted(zip(min_dist, img_with_min_dist))]
-                min_dist.sort()
-                # http://stackoverflow.com/questions/6618515/sorting-list-based-on-values-from-another-list
-
-
-            orientation = [0, 0, 0, 0]
-            for file_id in img_with_min_dist:
-                orientation[imf.train_files[file_id]["orient"]/90] += 1
-            """
-
             if dist < min_dist:
                 min_dist = dist
                 img_with_min_dist = train_f_id
 
-        # if imf.test_files[test_f_id]["orient"]/90 == orientation.index(min(orientation)):
         if imf.test_files[test_f_id]["orient"] == imf.train_files[img_with_min_dist]["orient"]:
             result += 1
         confusion_matrix[imf.test_files[test_f_id]["orient"]/90, imf.train_files[img_with_min_dist]["orient"]/90] += 1
@@ -87,12 +67,11 @@ test_file = argv[2]
 mode = argv[3]
 
 imf = ImageFiles()
-imf.train_files = read_files(train_file, True)
-imf.test_files = read_files(test_file, False)
+imf.train_files = read_files(train_file)
+imf.test_files = read_files(test_file)
 
 if mode == "nearest":
     test_nearest()
 
 end_time = time.time()
-print time.asctime(time.localtime(time.time()))
 print end_time - start_time
